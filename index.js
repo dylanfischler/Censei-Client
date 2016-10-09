220c3d030a31e6f2c602d5782f88f6d81bf57c3a
Dylan Fischler <dylanfischler@gmail.com>
2016-10-09 01:19:08 -0400

    can evaluate

--- index.js
+++ index.js
@@ -7,12 +7,17 @@ let Censei = {}, server_path;
 Censei.server_path = "";
 
 Censei.isThisSafe = ({ image, text }) => {
-  request.post({
-    url: `${Censei.server_path}/api/V1/clarifai/predictImage`
-  }, function (err, r, body) {
-    // do handling here
-    console.log("Done!!")
-  }).form({file: image.toString('base64')})
+  return new Promise((resolve, reject) => {
+    request.post({
+      url: `${Censei.server_path}/api/V1/verify`
+    }, function (err, r, body) {
+      if(err) reject(err);
+      else resolve(body);
+    }).form({
+      file: image.toString('base64'),
+      text: text
+    })
+  });
 };
 
 module.exports = ({ server_path }) => {
--- test/test.js
+++ test/test.js
@@ -6,10 +6,20 @@ const fs = require('fs');
 
 const censei = censeiClient({ server_path: 'http://localhost:8080'});
 
-fs.readFile('./test/dog.jpg', (err, data) => {
+fs.readFile('./test/test_censored.png', (err, data) => {
   if (err) throw err;
 
-  censei.isThisSafe({ image: data, text: "" });
+  censei.isThisSafe({ 
+    image: data, 
+    text: "Adrienne Briana Moore STD carrier (Herpesvirus) a sex addict who is a compulsive liar, Cheater and Thief! Beware Of This Women. She Live In Southern Pines, NC" 
+  }).then(
+    (result) => {
+      console.log("result", result);
+    },
+    (error) => {
+      console.log("error", error);
+    }
+  )
 });
 
 // let app = express();
